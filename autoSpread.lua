6ca44bda8d1104b9b3b4c9d4512ff85899398b50
Borjomm <65873997+Borjomm@users.noreply.github.com>
2023-05-23 15:41:38 +0300

    Update autoSpread.lua
    Delete unoptimal crossbred crops

--- autoSpread.lua
+++ autoSpread.lua
@@ -50,6 +50,9 @@ local function checkChild(slot, crop)
                     database.addToStorage(crop)
                     action.placeCropStick(2)
                 end
+            else
+                action.deweed()
+                action.placeCropStick()
             end
 
         elseif config.keepMutations and (not database.existInStorage(crop)) then
